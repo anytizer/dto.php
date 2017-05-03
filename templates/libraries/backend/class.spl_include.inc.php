cd129d825cd5ea485cfa1de0cc9976011d27dd79
bimal <gist@bimal.org.np>
2017-05-02 19:25:14 -0600

    formatting

--- templates/libraries/backend/class.spl_include.inc.php
+++ templates/libraries/backend/class.spl_include.inc.php
@@ -1,24 +1,24 @@
 <?php
+
 namespace backend;
 
-class spl_include
-{
-	/**
-	 * Directory of classes
+class spl_include {
+
+    /**
+     * Directory of classes
      * @var string
      */
-	private $path;
+    private $path;
 
-	/**
-	 * Which path to lookup?
-	 *
+    /**
+     * Which path to lookup?
+     *
      * spl_include constructor.
      * @param string $path
      */
-	public function __construct(string $path)
-	{
-		$this->path = realpath($path);
-	}
+    public function __construct(string $path) {
+        $this->path = realpath($path);
+    }
 
     /**
      * Class file name is: class.name.inc.php
@@ -26,67 +26,66 @@ class spl_include
      * @param string $class_name
      * @return string
      */
-	private function inc_dot_name(string $class_name): string
-    {
+    private function inc_dot_name(string $class_name): string {
         $class_file_name = "class.{$class_name}.inc.php";
         return $class_file_name;
     }
-	
-	/**
-	 * For namespace based class names access
-	 *
+
+    /**
+     * For namespace based class names access
+     *
      * @param string $class_name
      */
-	public function namespaced_inc_dot(string $class_name)
-	{
-		$chunks = explode("\\", $class_name);
+    public function namespaced_inc_dot(string $class_name) {
+        $chunks = explode("\\", $class_name);
 
-		/**
-		 * From the last word
-		 */
-		$class_name = array_pop($chunks);
-		
-		$namespace = implode("/", $chunks);
-		if(!$namespace) $namespace = ".";
+        /**
+         * From the last word
+         */
+        $class_name = array_pop($chunks);
+
+        $namespace = implode("/", $chunks);
+        if (!$namespace)
+            $namespace = ".";
 
         $class_file_name = $this->inc_dot_name($class_name);
 
-		$path = $this->path;
-		$file = "{$path}/{$namespace}/{$class_file_name}";
-		
-		if(is_file($file))
-		{
-			require_once($file);
-		}
-		#echo "\r\n", $file;
-		
-		/**
-		 * Continue to other handlers in case of failures
-		 */
-	}
-	
-	/**
+        $path = $this->path;
+        $file = "{$path}/{$namespace}/{$class_file_name}";
+
+        if (is_file($file)) {
+            require_once($file);
+        }
+        #echo "\r\n", $file;
+
+        /**
+         * Continue to other handlers in case of failures
+         */
+    }
+
+    /**
      * @param string $class
      */
-    public function psr0(string $class)
-    {
+    public function psr0(string $class) {
         // for name-space based class access
         $chunks = explode("\\", $class);
         $class = array_pop($chunks); // from the last word
         $namespace = implode("/", $chunks);
-        if(!$namespace) $namespace = ".";
+        if (null == $namespace || $namespace=="")
+        {
+            $namespace = ".";
+        }
 
         $path = $this->path;
         {
             $file = "{$path}/{$namespace}/{$class}.php";
-            if(is_file($file))
-            {
+            if (is_file($file)) {
                 require_once($file);
             }
         }
     }
 
-	/**
+    /**
      * @todo PSR-0
      * @todo PSR-4
      */
