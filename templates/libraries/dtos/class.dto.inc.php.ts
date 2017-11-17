ebe23d52a85a549c94e12be37aa80bfeac97e51a
bimal <gist@bimal.org.np>
2017-11-17 14:28:31 -0700

    disallow reading or writing dto members dynamically

--- templates/libraries/dtos/class.dto.inc.php.ts
+++ templates/libraries/dtos/class.dto.inc.php.ts
@@ -1,9 +1,22 @@
 <?php
 namespace dtos\dto;
 
+/**
+ * Unless otherwise defined, do not allow to read or write non-existing members
+ */
 abstract class dto
 {
     public function __construct()
     {
+    }
+	
+	public function __set($name, $value)
+	{
+		throw new Exception("Cannot add new property \$$name to instance of " . __CLASS__);
+    }
+	
+	public function __get($name)
+	{
+		throw new Exception("Invalid access \$$name to instance of " . __CLASS__);
     }
 }
