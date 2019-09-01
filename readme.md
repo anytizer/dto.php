0b7470254e6ab3c85b4616d0572aa0477fcaeea1
anytizer <anytizer@bimal.org.np>
2019-08-31 18:18:03 -0600

    readme

--- readme.md
+++ readme.md
@@ -1,29 +1,7 @@
-# DTO Generator
-
- - php -f business.php
-
- - dto table_name
- - Define features first (PHPUnit)
- - Write tests
- - Write codes
-
-Generates DTO Classes from SQL Database
-
-Convert to DTO directly from a table structure.
-Name must have DTO prepended.
-
-Classes cannot read or write anything extra.
+# Full stack source code generator
 
 ## Examples
 
     php -f business.php
 
-Class name: classname_dto
-File name: class.classname_dto.inc.php
-
-From sql to connection
-from connection to
- - DTOs
-
-dto thing_things things
-  - things.sql
+This is not a general purpose software.
--- test.php
+++ test.php
@@ -1,19 +1,32 @@
 <?php
-# D:\htdocs\laravel\orm
+# https://packagist.org/packages/illuminate/
+
+# composer global require illuminate/database
+# composer global require illuminate/events
+# composer global require illuminate/container
+
+# composer require illuminate/database
+# composer require illuminate/events
+# composer require illuminate/container
+
+# php.ini: include_paths
+
+require_once("vendor/autoload.php");
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Eloquent\Model as Model;
 
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'orm'  => 'test',
-    'username'  => 'root',
-    'password'  => '',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
+    "driver"    => "mysql",
+    "host"      => "localhost",
+    "database"  => "test",
+    "username"  => "root",
+    "password"  => "",
+    "charset"   => "utf8mb4",
+    "collation" => "utf8mb4_general_ci",
+    "prefix"    => "",
 ]);
 
 // Set the event dispatcher used by Eloquent business... (optional)
@@ -24,11 +37,40 @@ $capsule->setEventDispatcher(new Dispatcher(new Container));
 // Make this Capsule instance available globally via static methods... (optional)
 $capsule->setAsGlobal();
 
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+// Setup the Eloquent ORM... (optional; unless you"ve used setEventDispatcher())
 $capsule->bootEloquent();
 
-$users = Capsule::table('users')->where('votes', '>', 100)->get();
-$results = Capsule::select('SELECT * FROM users WHERE ID = ?', array(1));
+$users = Capsule::table("users")->where("votes", ">", 100)->get();
+$results = Capsule::select("SELECT * FROM users WHERE id = ?", array(1));
 
-class User extends Illuminate\Database\Eloquent\Model {}
-$users = User::where('votes', '>', 1)->get();
+// https://laravel.com/docs/5.6/eloquent
+class User extends Model {
+    protected $table = "users";
+    protected $primaryKey = "id";
+    public $timestamps = false;
+    protected $dateFormat = "U";
+    const CREATED_AT = "creation_date";
+    const UPDATED_AT = "last_update";
+    protected $guarded = ["price"];
+    // protected $connection = "connection-name";
+}
+
+$user = new User;
+$user->name = "Test";
+$user->votes = 2;
+$user->save();
+
+$users = User::where("votes", ">", 1)->get();
+print_r($users);
+
+$user = User::find(1);
+echo $user->name = "New Flight Name";
+$user->save();
+$user->delete();
+
+# $deletedRows = App\Flight::where("active", 0)->delete();
+
+#$flight = App\Flight::firstOrCreate(["name" => "Flight 10"]);
+#$flight = App\Flight::firstOrNew(["name" => "Flight 10"]);
+#$flight = App\Flight::updateOrCreate()
+#-- CREATE TABLE users (id INT AUTO_INCREMENT, `name` VARCHAR(255) NOT NULL DEFAULT "", votes INT, PRIMARY KEY(id));
